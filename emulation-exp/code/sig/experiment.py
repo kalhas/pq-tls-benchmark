a486afd6741a691fe294c551bf24f496abe3c0f7
Herc <hercoriginal@gmail.com>
2020-10-13 21:37:49 +0300

    more fixes2

--- emulation-exp/code/sig/experiment.py
+++ emulation-exp/code/sig/experiment.py
@@ -5,10 +5,10 @@ import subprocess
 import sys
 
 # Our experiment used POOL_SIZE = 40
-POOL_SIZE = 4
+POOL_SIZE = 8
 
-MEASUREMENTS_PER_TIMER = 1
-TIMERS = 4
+MEASUREMENTS_PER_TIMER = 10
+TIMERS = 8
 
 def run_subprocess(command, working_dir='.', expected_returncode=0):
     result = subprocess.run(
@@ -87,14 +87,7 @@ for latency_ms in ['2.684ms', '15.458ms', '39.224ms', '97.73ms']:
 
     with open('data/{}_{}ms.csv'.format(sig_alg, rtt_str),'w') as out:
         csv_out=csv.writer(out)
-        for pkt_loss in [0, 0.1, 0.5, 1, 1.5, 2, 2.5, 3]:
-            change_qdisc('cli_ns', 'cli_ve', pkt_loss, delay=latency_ms)
-            change_qdisc('srv_ns', 'srv_ve', pkt_loss, delay=latency_ms)
-            result = run_timers(sig_alg, timer_pool)
-            result.insert(0, pkt_loss)
-            csv_out.writerow(result)
-
-        for pkt_loss in range(4, 21):
+        for pkt_loss in [0, 0.1, 0.5, 1, 1.5, 2, 2.5, 3, 4, 5]:
             change_qdisc('cli_ns', 'cli_ve', pkt_loss, delay=latency_ms)
             change_qdisc('srv_ns', 'srv_ve', pkt_loss, delay=latency_ms)
             result = run_timers(sig_alg, timer_pool)
