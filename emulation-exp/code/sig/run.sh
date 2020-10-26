8232a3b4bb8b346d97a8daa8f7c80a8d6029d9a3
Herc <hercoriginal@gmail.com>
2020-10-26 15:43:42 +0200

    extra ciphers

--- emulation-exp/code/sig/run.sh
+++ emulation-exp/code/sig/run.sh
@@ -13,7 +13,7 @@ ip netns exec srv_ns ${NGINX_APP} -s stop
 ##########################
 set -e
 
-for SIG in "ecdsap256" "dilithium2" "rainbowIaclassic" "falcon512";
+for SIG in "ecdsap256" "dilithium2" "dilithium3" "dilithium4" "falcon512" "falcon1024" "rainbowIaclassic"  "p256_dilithium2"  "p384_dilithium3"  "p521_dilithium4" "p256_falcon512"  "p521_falcon1024" "p256_rainbowIaclassic";
 do
     # Ask nginx to use ${SIG} cert and key
     sed "s/??SERVER_CERT??/${SIG}_server.crt/g; s/??SERVER_KEY??/${SIG}_server.key/g" nginx.conf > ${NGINX_CONF_DIR}/nginx.conf
